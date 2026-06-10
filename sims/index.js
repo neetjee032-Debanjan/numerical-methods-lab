2599d4f0666f1f180a6b119627083a269c1dd624
neetjee032-Debanjan <neetjee032@gmail.com>
2026-06-11 02:10:35 +0530

    Update index.js

--- sims/index.js
+++ sims/index.js
@@ -1,25 +1,40 @@
 import { runNewton } from "./newton-raphson.js";
 import { runBisection } from "./bisection.js";
-import { runIntegration } from "./integration.js";
-import { runLagrange } from "./lagrange.js";
-import { runDE } from "./differential.js";
+import { runSecant } from "./secant.js";
 import { runFalsePosition } from "./falsePosition.js";
 
+import { runIntegration } from "./integration.js";
+
+import { runLagrange } from "./lagrange.js";
+
+import { runDE } from "./differential.js";
+
 export const Simulations = {
+
+  /* ROOT FINDING */
 
   "newton": runNewton,
   "newton-raphson": runNewton,
 
   "bisection": runBisection,
 
+  "secant": runSecant,
+
   "falseposition": runFalsePosition,
   "false-position": runFalsePosition,
   "regulafalsi": runFalsePosition,
 
+  /* INTEGRATION */
+
   "integration": runIntegration,
   "trapezoid": runIntegration,
+  "trapezoidal": runIntegration,
+
+  /* INTERPOLATION */
 
   "lagrange": runLagrange,
+
+  /* DIFFERENTIAL EQUATIONS */
 
   "differential": runDE,
   "euler": runDE
