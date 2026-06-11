6f7765301a82c4a058a1419af3b639dc36379301
neetjee032-Debanjan <neetjee032@gmail.com>
2026-06-11 19:49:02 +0530

    Update index.js

--- sims/index.js
+++ sims/index.js
@@ -1,41 +1,39 @@
 import { runNewton } from "./newton-raphson.js";
 import { runBisection } from "./bisection.js";
-import { runSecant } from "./secant.js";
-import { runFalsePosition } from "./falsePosition.js";
-
 import { runIntegration } from "./integration.js";
-
 import { runLagrange } from "./lagrange.js";
-
 import { runDE } from "./differential.js";
+
+import { runFalsePosition } from "./falsePosition.js";
+import { runSecant } from "./secant.js";
+import { runSimpson13 } from "./simpson13.js";
 
 export const Simulations = {
 
-  /* ROOT FINDING */
-
+  /* Root Finding */
   "newton": runNewton,
   "newton-raphson": runNewton,
 
   "bisection": runBisection,
 
-  "secant": runSecant,
-
   "falseposition": runFalsePosition,
   "false-position": runFalsePosition,
   "regulafalsi": runFalsePosition,
 
-  /* INTEGRATION */
+  "secant": runSecant,
 
-  "integration": runIntegration,
-  "trapezoid": runIntegration,
-  "trapezoidal": runIntegration,
-
-  /* INTERPOLATION */
-
+  /* Interpolation */
   "lagrange": runLagrange,
 
-  /* DIFFERENTIAL EQUATIONS */
+  /* Integration */
+  "integration": runIntegration,
+  "trapezoid": runIntegration,
 
+  "simpson13": runSimpson13,
+  "simpson-1-3": runSimpson13,
+  "simpson": runSimpson13,
+
+  /* Differential Equations */
   "differential": runDE,
   "euler": runDE
 
