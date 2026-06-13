10e17ef2d4ede633ed6ee0fc2b3840f3d79a54ee
neetjee032-Debanjan <neetjee032@gmail.com>
2026-06-13 17:29:15 +0530

    Update quizzes.js

--- data/quizzes.js
+++ data/quizzes.js
@@ -1,1 +1,81 @@
-export const quizzes = {};
+export const quizzes = {
+
+  l1: [
+
+    {
+      question:
+        "Why is floating-point representation used in computers?",
+
+      options: [
+        "To store only integers",
+        "To represent very large and very small numbers efficiently",
+        "To increase CPU speed",
+        "To replace binary arithmetic"
+      ],
+
+      answer: 1
+    },
+
+    {
+      question:
+        "Floating-point representation is inspired by:",
+
+      options: [
+        "Boolean algebra",
+        "Scientific notation",
+        "Hexadecimal notation",
+        "Matrix algebra"
+      ],
+
+      answer: 1
+    },
+
+    {
+      question:
+        "Which component determines the scale of a floating-point number?",
+
+      options: [
+        "Mantissa",
+        "Exponent",
+        "Sign bit",
+        "Fraction"
+      ],
+
+      answer: 1
+    }
+
+  ],
+
+  l2: [
+
+    {
+      question:
+        "Absolute error is:",
+
+      options: [
+        "|True Value − Approximate Value|",
+        "True Value / Approximate Value",
+        "Approximate Value / True Value",
+        "Percentage error"
+      ],
+
+      answer: 0
+    },
+
+    {
+      question:
+        "Round-off error is mainly caused by:",
+
+      options: [
+        "Finite precision",
+        "Infinite memory",
+        "Compiler bugs",
+        "Operating systems"
+      ],
+
+      answer: 0
+    }
+
+  ]
+
+};
