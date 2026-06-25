df9f35b67b052f8010e55b4896b05309571b2673
neetjee032-Debanjan <neetjee032@gmail.com>
2026-06-26 02:47:52 +0530

    Create resources.js

---
+++ data/resources.js
@@ -0,0 +1,142 @@
+export const resources = [
+
+  {
+    module: "Module 1",
+    title: "Root Finding Methods",
+    icon: "🧮",
+
+    topics: [
+
+      "Bisection Method",
+      "False Position",
+      "Newton-Raphson",
+      "Secant Method",
+      "Fixed Point Iteration"
+
+    ],
+
+    resources: {
+
+      notes: [],
+      videos: [],
+      formulas: [],
+      books: [],
+      practice: [],
+      code: [],
+      simulators: []
+
+    }
+
+  },
+
+  {
+    module: "Module 2",
+    title: "Interpolation",
+    icon: "📈",
+
+    topics: [
+
+      "Lagrange",
+      "Newton Forward",
+      "Newton Backward",
+      "Divided Difference",
+      "Cubic Spline"
+
+    ],
+
+    resources: {
+
+      notes: [],
+      videos: [],
+      formulas: [],
+      books: [],
+      practice: [],
+      code: [],
+      simulators: []
+
+    }
+
+  },
+
+  {
+    module: "Module 3",
+    title: "Numerical Differentiation",
+    icon: "📉",
+
+    topics: [
+
+      "Forward Difference",
+      "Backward Difference",
+      "Central Difference"
+
+    ],
+
+    resources: {
+
+      notes: [],
+      videos: [],
+      formulas: [],
+      books: [],
+      practice: [],
+      code: [],
+      simulators: []
+
+    }
+
+  },
+
+  {
+    module: "Module 4",
+    title: "Numerical Integration",
+    icon: "∫",
+
+    topics: [
+
+      "Trapezoidal Rule",
+      "Simpson 1/3",
+      "Simpson 3/8"
+
+    ],
+
+    resources: {
+
+      notes: [],
+      videos: [],
+      formulas: [],
+      books: [],
+      practice: [],
+      code: [],
+      simulators: []
+
+    }
+
+  },
+
+  {
+    module: "Module 5",
+    title: "Ordinary Differential Equations",
+    icon: "📐",
+
+    topics: [
+
+      "Euler Method",
+      "Modified Euler",
+      "Runge-Kutta"
+
+    ],
+
+    resources: {
+
+      notes: [],
+      videos: [],
+      formulas: [],
+      books: [],
+      practice: [],
+      code: [],
+      simulators: []
+
+    }
+
+  }
+
+];
