3c97b5d20b10a0049a88ce0d2fdbaaac9160a057
neetjee032-Debanjan <neetjee032@gmail.com>
2026-06-27 02:32:00 +0530

    Update resources.js

--- data/resources.js
+++ data/resources.js
@@ -6,14 +6,14 @@ import { ode } from "./resources/ode.js";
 
 export const resources = [
 
-rootFinding,
+  rootFinding,
 
-interpolation,
+  interpolation,
 
-differentiation,
+  differentiation,
 
-integration,
+  integration,
 
-ode
+  ode
 
 ];
