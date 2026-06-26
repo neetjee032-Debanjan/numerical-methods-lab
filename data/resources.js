8edf5186bebf955355f146217ba1c8093eccff07
neetjee032-Debanjan <neetjee032@gmail.com>
2026-06-27 02:13:38 +0530

    Update resources.js

--- data/resources.js
+++ data/resources.js
@@ -1,142 +1,19 @@
+import { rootFinding } from "./resources/rootFinding.js";
+import { interpolation } from "./resources/interpolation.js";
+import { differentiation } from "./resources/differentiation.js";
+import { integration } from "./resources/integration.js";
+import { ode } from "./resources/ode.js";
+
 export const resources = [
 
-  {
-    module: "Module 1",
-    title: "Root Finding Methods",
-    icon: "🧮",
+rootFinding,
 
-    topics: [
+interpolation,
 
-      "Bisection Method",
-      "False Position",
-      "Newton-Raphson",
-      "Secant Method",
-      "Fixed Point Iteration"
+differentiation,
 
-    ],
+integration,
 
-    resources: {
-
-      notes: [],
-      videos: [],
-      formulas: [],
-      books: [],
-      practice: [],
-      code: [],
-      simulators: []
-
-    }
-
-  },
-
-  {
-    module: "Module 2",
-    title: "Interpolation",
-    icon: "📈",
-
-    topics: [
-
-      "Lagrange",
-      "Newton Forward",
-      "Newton Backward",
-      "Divided Difference",
-      "Cubic Spline"
-
-    ],
-
-    resources: {
-
-      notes: [],
-      videos: [],
-      formulas: [],
-      books: [],
-      practice: [],
-      code: [],
-      simulators: []
-
-    }
-
-  },
-
-  {
-    module: "Module 3",
-    title: "Numerical Differentiation",
-    icon: "📉",
-
-    topics: [
-
-      "Forward Difference",
-      "Backward Difference",
-      "Central Difference"
-
-    ],
-
-    resources: {
-
-      notes: [],
-      videos: [],
-      formulas: [],
-      books: [],
-      practice: [],
-      code: [],
-      simulators: []
-
-    }
-
-  },
-
-  {
-    module: "Module 4",
-    title: "Numerical Integration",
-    icon: "∫",
-
-    topics: [
-
-      "Trapezoidal Rule",
-      "Simpson 1/3",
-      "Simpson 3/8"
-
-    ],
-
-    resources: {
-
-      notes: [],
-      videos: [],
-      formulas: [],
-      books: [],
-      practice: [],
-      code: [],
-      simulators: []
-
-    }
-
-  },
-
-  {
-    module: "Module 5",
-    title: "Ordinary Differential Equations",
-    icon: "📐",
-
-    topics: [
-
-      "Euler Method",
-      "Modified Euler",
-      "Runge-Kutta"
-
-    ],
-
-    resources: {
-
-      notes: [],
-      videos: [],
-      formulas: [],
-      books: [],
-      practice: [],
-      code: [],
-      simulators: []
-
-    }
-
-  }
+ode
 
 ];
